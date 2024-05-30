47600e7ae5c055e91e5449d62e0b2c8946b6bd67
Daniel Ostermeier <ostermeier_Daniel@t-online.de>
2024-05-31 00:22:11 +0200

    Different Approach; Facing errors

--- src/IK/3R_IK.cpp
+++ src/IK/3R_IK.cpp
@@ -16,28 +16,77 @@ namespace IKS
     IK_Solution General_3R::calculate_IK(const Homogeneous_T &ee_position_orientation) const
     {
         IK_Solution solution;
+        IK_Solution solution_t_12;
         const Eigen::Vector3d p_0t = ee_position_orientation.block<3, 1>(0, 3);
         const Eigen::Matrix3d r_03 = ee_position_orientation.block<3, 3>(0, 0);
-        
-        // Solving 3R kinematics by orientation constraints:
-        SP4 sp4(this->H.col(0),
-                this->H.col(2),
-                this->H.col(1),
-                this->H.col(0).transpose() * r_03 * this->H.col(2));
-        sp4.solve();
+        const Eigen::Vector3d p_03 = p_0t;// - r_03*this->P.col(3);        
 
-        const std::vector<double> &theta_2 = sp4.get_theta();
-
-        for (const auto &q2 : theta_2)
+        // Checking for parallel Axes
+        if (this->H.col(0).cross(this->H.col(1)).norm() < ZERO_THRESH)
         {
-            const Eigen::Matrix3d r12 = Eigen::AngleAxisd(q2, this->H.col(1).normalized()).toRotationMatrix();
-            SP1 sp1_t1 (r_03*this->H.col(2), r12*this->H.col(2), -this->H.col(0));
-            sp1_t1.solve();
+            // Axis 1 || Axis 2
+            SP3 sp3(this->P.col(2), -this->P.col(1), this->H.col(1), p_03.norm());
+            sp3.solve();
 
-            SP1 sp1_t3 (r12.transpose()*this->H.col(0), r_03.transpose()*this->H.col(0), -this->H.col(2));
-            sp1_t3.solve();
-            solution.Q.push_back({sp1_t1.get_theta(), q2, sp1_t3.get_theta()});
-            solution.is_LS_vec.push_back(sp1_t1.solution_is_ls()||sp1_t3.solution_is_ls()||sp4.solution_is_ls());
+            const std::vector<double> &theta_2 = sp3.get_theta();
+
+            for (const auto &q2 : theta_2)
+            {
+                const Eigen::Matrix3d r12 = Eigen::AngleAxisd(q2, this->H.col(1).normalized()).toRotationMatrix();
+                SP1 sp1(p_03, this->P.col(1)+r12*this->P.col(2), -this->H.col(0));
+                sp1.solve();
+
+                solution_t_12.Q.push_back({sp1.get_theta(), q2});
+                solution_t_12.is_LS_vec.push_back(sp3.solution_is_ls()||sp1.solution_is_ls());
+            }
+        }
+        else if (std::fabs(this->H.col(0).cross(this->H.col(1)).transpose() * this->P.col(1)) < ZERO_THRESH)
+        {
+            // (h1 x h2)(p12)==0) -> h1 intersects h2
+            SP2 sp2(p_03, this->P.col(2), -this->H.col(0), this->H.col(1));
+            sp2.solve();
+
+            for(unsigned i = 0; i < sp2.get_theta_1().size(); i++)
+            {
+                const double q1 = sp2.get_theta_1().at(i);
+                const double q2 = sp2.get_theta_2().at(i);
+                solution_t_12.Q.push_back({q1, q2});
+                solution_t_12.is_LS_vec.push_back(sp2.solution_is_ls());
+            }
+        }
+        else
+        {
+            double d = this->H.col(0).transpose()*(p_03-this->P.col(1));
+            SP4 sp4(this->H.col(0), this->P.col(2), this->H.col(1), d);
+            sp4.solve();
+
+            const std::vector<double> &theta_2 = sp4.get_theta();
+
+            for (const auto &q2 : theta_2)
+            {
+                const Eigen::Matrix3d r12 = Eigen::AngleAxisd(q2, this->H.col(1).normalized()).toRotationMatrix();
+                SP1 sp1(p_03, this->P.col(1)+r12*this->P.col(2), -this->H.col(0));
+                sp1.solve();
+
+                solution_t_12.Q.push_back({sp1.get_theta(), q2});
+                solution_t_12.is_LS_vec.push_back(sp4.solution_is_ls()||sp1.solution_is_ls());
+            }
+        }
+
+        for(unsigned i = 0; i < solution_t_12.Q.size(); i++)
+        {
+            const double q1 = solution_t_12.Q.at(i).at(0);
+            const double q2 = solution_t_12.Q.at(i).at(1);
+
+            const Eigen::Matrix3d r12 = Eigen::AngleAxisd(q2, this->H.col(1).normalized()).toRotationMatrix();
+            const Eigen::Matrix3d r01 = Eigen::AngleAxisd(q1, this->H.col(0).normalized()).toRotationMatrix();
+
+            const Eigen::Vector3d hn = this->H.col(2).cross(Eigen::Vector3d(1,0,0)); // Vector that's not collinear to h3
+
+            SP1 sp1(hn, r12.transpose()*r01.transpose()*r_03*hn, this->H.col(2));
+            sp1.solve();
+            solution.Q.push_back({q1, q2, sp1.get_theta()});
+            solution.is_LS_vec.push_back(solution_t_12.is_LS_vec.at(i)|sp1.solution_is_ls());
         }
 
         // Finding 6DOF Problem for 3R is overconstrained -> "Throw away" extraneous orientation solutions
@@ -50,6 +99,8 @@ namespace IKS
 
                 if(error > ZERO_THRESH)
                 {
+                    std::cout<< result<<std::endl;
+                    std::cout<< ee_position_orientation<<std::endl;
                     solution.is_LS_vec.at(i) = true;
                 }
             }
