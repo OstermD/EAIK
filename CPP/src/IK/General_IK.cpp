333fc396c213fabbd151ae7190ed149732a2db88
Daniel Ostermeier <ostermeier_Daniel@t-online.de>
2024-11-15 10:00:41 +0100

    Revise kinematic class selection for 6R

--- CPP/src/IK/General_IK.cpp
+++ CPP/src/IK/General_IK.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
+#include <memory>
 
 #include "sp.h"
 #include "IKS.h"
 #include "utils/kinematic_utils.h"
+#include "kinematic_remodelling.h"
 
 namespace IKS
 {
@@ -54,9 +56,11 @@ namespace IKS
     General_6R::General_6R(const Eigen::Matrix<double, 3, 6> &H, const Eigen::Matrix<double, 3, 7> &P)
         : General_Robot(H,P), H(H), P(P)
     {
+        this->kinematicClass = determine_Kinematic_Class();
+        std::cout<< "kin class "<< kinematicClass<<std::endl;
     }
 
-    bool General_6R::has_known_decomposition() const
+    General_6R::KinematicClass General_6R::determine_Kinematic_Class()
     {
         // Check for parallel axes
         if (this->H.col(0).cross(this->H.col(1)).norm() < ZERO_THRESH &&
@@ -69,36 +73,53 @@ namespace IKS
             if(this->H.col(4).cross(this->H.col(5)).norm() >= ZERO_THRESH && 
             std::fabs(this->H.col(4).cross(this->H.col(5)).transpose() * this->P.col(5)) < ZERO_THRESH)
             {
-                return true;
+                return KinematicClass::THREE_PARALLEL_TWO_INTERSECTING;
             }
         }
-        else if (this->H.col(1).cross(this->H.col(2)).norm() < ZERO_THRESH &&
+
+        if (this->H.col(1).cross(this->H.col(2)).norm() < ZERO_THRESH &&
                  this->H.col(1).cross(this->H.col(3)).norm() < ZERO_THRESH &&
                  this->H.col(2).cross(this->H.col(3)).norm() < ZERO_THRESH)
         {
             // h2 || h3 || h4
-            return true;
+            return KinematicClass::THREE_INNER_PARALLEL;
         }
-        else if (this->H.col(2).cross(this->H.col(3)).norm() < ZERO_THRESH &&
+        
+        if (this->H.col(2).cross(this->H.col(3)).norm() < ZERO_THRESH &&
                  this->H.col(2).cross(this->H.col(4)).norm() < ZERO_THRESH &&
                  this->H.col(3).cross(this->H.col(4)).norm() < ZERO_THRESH)
         {
             // h3 || h4 || h5
-            return true;
+            const auto&[H_reversed, P_reversed] = reverse_kinematic_chain(this->H, this->P);
+            const Eigen::MatrixXd P_reversed_remodelled = EAIK::remodel_kinematics(H_reversed, P_reversed, ZERO_THRESH, ZERO_THRESH);
+
+            this->reversed_Robot_ptr = std::make_unique<General_6R>(H_reversed, P_reversed_remodelled);
+            return KinematicClass::THREE_INNER_PARALLEL_REVERSED;
         }
-        else if (this->H.col(3).cross(this->H.col(4)).norm() < ZERO_THRESH &&
+        
+        if (this->H.col(3).cross(this->H.col(4)).norm() < ZERO_THRESH &&
                  this->H.col(3).cross(this->H.col(5)).norm() < ZERO_THRESH &&
                  this->H.col(4).cross(this->H.col(5)).norm() < ZERO_THRESH)
         {
             // h4 || h5 || h6
+            // (h1 x h1)(p12)==0) -> h1 intersects h1
+            // And 1 not parallel to 2 (h1 x h2 =/= 0)
             if(this->H.col(0).cross(this->H.col(1)).norm() >= ZERO_THRESH && 
             std::fabs(this->H.col(0).cross(this->H.col(1)).transpose() * this->P.col(1)) < ZERO_THRESH)
             {
-                return true;
+                // h3 || h4 || h5
+                const auto&[H_reversed, P_reversed] = reverse_kinematic_chain(this->H, this->P);
+                this->reversed_Robot_ptr = std::make_unique<General_6R>(H_reversed, P_reversed);
+                return KinematicClass::THREE_PARALLEL_TWO_INTERSECTING_REVERSED;
             }
         }
 
-        return false;
+        return KinematicClass::UNKNOWN;   
+    }
+
+    bool General_6R::has_known_decomposition() const
+    {
+        return this->kinematicClass != KinematicClass::UNKNOWN;
     }
 
     IK_Solution General_6R::calculate_IK(const Homogeneous_T &ee_position_orientation) const
@@ -112,17 +133,9 @@ namespace IKS
         // Calculate "Position-IK":
         std::vector<std::vector<double>> position_solutions;
 
-        // Check for parallel axes
-        if (this->H.col(0).cross(this->H.col(1)).norm() < ZERO_THRESH &&
-            this->H.col(0).cross(this->H.col(2)).norm() < ZERO_THRESH &&
-            this->H.col(1).cross(this->H.col(2)).norm() < ZERO_THRESH)
+        switch(this->kinematicClass)
         {
-            // h1 || h2 || h3 -> first three axes parallel
-
-            // (h5 x h6)(p56)==0) -> h5 intersects h6
-            // And 5 not parallel to 6 (h5 x h6 =/= 0)
-            if(this->H.col(4).cross(this->H.col(5)).norm() >= ZERO_THRESH && 
-            std::fabs(this->H.col(4).cross(this->H.col(5)).transpose() * this->P.col(5)) < ZERO_THRESH)
+            case KinematicClass::THREE_PARALLEL_TWO_INTERSECTING:
             {
                 const double d = this->H.col(0).transpose()*(p_16 - this->P.col(1) - this->P.col(2) - this->P.col(3));
                 SP4 sp4_t4(this->H.col(0),this->P.col(4), this->H.col(3), d);
@@ -171,136 +184,137 @@ namespace IKS
                         }
                     }
                 }
-            }
-            else
+                break;
+            };
+            case KinematicClass::THREE_PARALLEL_TWO_INTERSECTING_REVERSED:
             {
-                throw std::runtime_error("This manipulator type (Axis 1,2,3 parallel with no additional parallel axes) is not yet solvable by EAIK.");
-            }
-        }
-        else if (this->H.col(1).cross(this->H.col(2)).norm() < ZERO_THRESH &&
-                 this->H.col(1).cross(this->H.col(3)).norm() < ZERO_THRESH &&
-                 this->H.col(2).cross(this->H.col(3)).norm() < ZERO_THRESH)
-        {
-            // h2 || h3 || h4
-            std::vector<double> theta_1;
-            std::vector<double> theta_5;
-            std::vector<bool> is_ls_q1_q5;
+                if(reversed_Robot_ptr)
+                {
+                    // Use reversed kinematic chain and calculate IK for robot with h1 || h2 || h3
+                    solution = this->reversed_Robot_ptr->calculate_IK(inverse_homogeneous_T(ee_position_orientation));
+                    reverse_vector_second_dimension(solution.Q);
+                }
+                else
+                {
+                    throw std::runtime_error("Reversed kinematic chain was not initialized! Please open an issue on our GitHub repository.");
+                }
+                break;
+            };
+            case KinematicClass::THREE_INNER_PARALLEL:
+            {
+                std::vector<double> theta_1;
+                std::vector<double> theta_5;
+                std::vector<bool> is_ls_q1_q5;
+                
+                // (h5 x h6)(p56)==0) -> h5 intersects h6
+                // And 5 not parallel to 6 (h5 x h6 =/= 0)
+                if(this->H.col(4).cross(this->H.col(5)).norm() >= ZERO_THRESH && 
+                std::fabs(this->H.col(4).cross(this->H.col(5)).transpose() * this->P.col(5)) < ZERO_THRESH
+                )
+                { 
+                    const Eigen::Vector3d p15 = this->P.col(1)+this->P.col(2)+this->P.col(3)+this->P.col(4);
+                    SP4 sp4_q1(this->H.col(1), p_16, -this->H.col(0), this->H.col(1).transpose()*p15);
+                    sp4_q1.solve();
+                    theta_1 = sp4_q1.get_theta();
+                    std::vector<double> theta_1_new;
 
-            // (h5 x h6)(p56)==0) -> h5 intersects h6
-            // And 5 not parallel to 6 (h5 x h6 =/= 0)
-            if(this->H.col(4).cross(this->H.col(5)).norm() >= ZERO_THRESH && 
-            std::fabs(this->H.col(4).cross(this->H.col(5)).transpose() * this->P.col(5)) < ZERO_THRESH
-            )
-            { 
-                const Eigen::Vector3d p15 = this->P.col(1)+this->P.col(2)+this->P.col(3)+this->P.col(4);
-                SP4 sp4_q1(this->H.col(1), p_16, -this->H.col(0), this->H.col(1).transpose()*p15);
-                sp4_q1.solve();
-                theta_1 = sp4_q1.get_theta();
-                std::vector<double> theta_1_new;
+                    for(unsigned i = 0; i < theta_1.size(); i++)
+                    {
+                        const double& q1 = theta_1.at(i);
+                        const Eigen::Matrix3d r_01 = Eigen::AngleAxisd(q1, this->H.col(0).normalized()).toRotationMatrix();
+
+                        SP4 sp4_q5(this->H.col(1), this->H.col(5), this->H.col(4), this->H.col(1).transpose()*(r_01.transpose())*r_06*this->H.col(5));
+                        sp4_q5.solve();
+                        theta_5.insert(theta_5.end(), sp4_q5.get_theta().begin(), sp4_q5.get_theta().end());
+                        std::vector<double> q1_temp(sp4_q5.get_theta().size(), q1);
+                        theta_1_new.insert(theta_1_new.end(), q1_temp.begin(), q1_temp.end());
+
+                        std::vector<bool> is_ls_temp(sp4_q5.get_theta().size(), sp4_q1.solution_is_ls()||sp4_q5.solution_is_ls());
+                        is_ls_q1_q5.insert(is_ls_q1_q5.end(), is_ls_temp.begin(), is_ls_temp.end());
+                    }
+                    theta_1 = theta_1_new;
+                }
+                else
+                {
+                    const double d1 = this->H.col(1).transpose() * (this->P.col(2) + this->P.col(3) + this->P.col(4) + this->P.col(1));
+                    const double d2 = 0;
+
+                    SP6 sp6(this->H.col(1),
+                            this->H.col(1),
+                            this->H.col(1),
+                            this->H.col(1),
+                            -this->H.col(0),
+                            this->H.col(4),
+                            -this->H.col(0),
+                            this->H.col(4),
+                            p_16,
+                            -this->P.col(5),
+                            r_06 * this->H.col(5),
+                            -this->H.col(5),
+                            d1,
+                            d2);
+
+                    sp6.solve();
+                    theta_1 = sp6.get_theta_1();
+                    theta_5 = sp6.get_theta_2();
+
+                    is_ls_q1_q5 = std::vector(theta_1.size(), sp6.solution_is_ls());
+                }
 
                 for(unsigned i = 0; i < theta_1.size(); i++)
                 {
                     const double& q1 = theta_1.at(i);
+                    const double& q5 = theta_5.at(i);
+
                     const Eigen::Matrix3d r_01 = Eigen::AngleAxisd(q1, this->H.col(0).normalized()).toRotationMatrix();
+                    const Eigen::Matrix3d r_45 = Eigen::AngleAxisd(q5, this->H.col(4).normalized()).toRotationMatrix();
 
-                    SP4 sp4_q5(this->H.col(1), this->H.col(5), this->H.col(4), this->H.col(1).transpose()*(r_01.transpose())*r_06*this->H.col(5));
-                    sp4_q5.solve();
-                    theta_5.insert(theta_5.end(), sp4_q5.get_theta().begin(), sp4_q5.get_theta().end());
-                    std::vector<double> q1_temp(sp4_q5.get_theta().size(), q1);
-                    theta_1_new.insert(theta_1_new.end(), q1_temp.begin(), q1_temp.end());
+                    SP1 sp_14(r_45*this->H.col(5), r_01.transpose()*r_06*this->H.col(5), this->H.col(1));
+                    SP1 sp_q6(r_45.transpose()*this->H.col(1), r_06.transpose()*r_01*this->H.col(1), -this->H.col(5));
+                    sp_14.solve();
 
-                    std::vector<bool> is_ls_temp(sp4_q5.get_theta().size(), sp4_q1.solution_is_ls()||sp4_q5.solution_is_ls());
-                    is_ls_q1_q5.insert(is_ls_q1_q5.end(), is_ls_temp.begin(), is_ls_temp.end());
+                    const Eigen::Matrix3d r_14 = Eigen::AngleAxisd(sp_14.get_theta(), this->H.col(1).normalized()).toRotationMatrix();
+                    const Eigen::Vector3d d_inner = r_01.transpose()*p_16-this->P.col(1) - r_14*r_45*this->P.col(5) - r_14*this->P.col(4);
+                    const double d = d_inner.norm();
+
+                    SP3 sp3_t3(-this->P.col(3), this->P.col(2), this->H.col(1), d);
+                    sp3_t3.solve();
+                    sp_q6.solve();
+
+                    const std::vector<double> theta_3 = sp3_t3.get_theta();
+
+                    for(const auto& q3 : theta_3)
+                    {                
+                        const Eigen::Matrix3d rot_1 = Eigen::AngleAxisd(q3, this->H.col(1).normalized()).toRotationMatrix();
+                        SP1 sp1_q2(this->P.col(2)+rot_1*this->P.col(3), d_inner, this->H.col(1));
+                        sp1_q2.solve();
+
+                        double q4 = sp_14.get_theta() - sp1_q2.get_theta() - q3;
+                        q4 = std::atan2(std::sin(q4), std::cos(q4)); // Map angle q4 to [-PI,PI)
+
+                        solution.Q.push_back({q1, sp1_q2.get_theta(), q3, q4, q5, sp_q6.get_theta()});
+                        solution.is_LS_vec.push_back(is_ls_q1_q5.at(i) || sp1_q2.solution_is_ls() || sp3_t3.solution_is_ls() ||sp_q6.solution_is_ls());
+                    }
                 }
-                theta_1 = theta_1_new;
-            }
-            else
+                break;
+            };
+            case KinematicClass::THREE_INNER_PARALLEL_REVERSED:
             {
-                const double d1 = this->H.col(1).transpose() * (this->P.col(2) + this->P.col(3) + this->P.col(4) + this->P.col(1));
-                const double d2 = 0;
-
-                SP6 sp6(this->H.col(1),
-                        this->H.col(1),
-                        this->H.col(1),
-                        this->H.col(1),
-                        -this->H.col(0),
-                        this->H.col(4),
-                        -this->H.col(0),
-                        this->H.col(4),
-                        p_16,
-                        -this->P.col(5),
-                        r_06 * this->H.col(5),
-                        -this->H.col(5),
-                        d1,
-                        d2);
-
-                sp6.solve();
-                theta_1 = sp6.get_theta_1();
-                theta_5 = sp6.get_theta_2();
-
-                is_ls_q1_q5 = std::vector(theta_1.size(), sp6.solution_is_ls());
-            }
-
-            for(unsigned i = 0; i < theta_1.size(); i++)
-            {
-                const double& q1 = theta_1.at(i);
-                const double& q5 = theta_5.at(i);
-
-                const Eigen::Matrix3d r_01 = Eigen::AngleAxisd(q1, this->H.col(0).normalized()).toRotationMatrix();
-                const Eigen::Matrix3d r_45 = Eigen::AngleAxisd(q5, this->H.col(4).normalized()).toRotationMatrix();
-
-                SP1 sp_14(r_45*this->H.col(5), r_01.transpose()*r_06*this->H.col(5), this->H.col(1));
-                SP1 sp_q6(r_45.transpose()*this->H.col(1), r_06.transpose()*r_01*this->H.col(1), -this->H.col(5));
-                sp_14.solve();
-
-                const Eigen::Matrix3d r_14 = Eigen::AngleAxisd(sp_14.get_theta(), this->H.col(1).normalized()).toRotationMatrix();
-                const Eigen::Vector3d d_inner = r_01.transpose()*p_16-this->P.col(1) - r_14*r_45*this->P.col(5) - r_14*this->P.col(4);
-                const double d = d_inner.norm();
-
-                SP3 sp3_t3(-this->P.col(3), this->P.col(2), this->H.col(1), d);
-                sp3_t3.solve();
-                sp_q6.solve();
-
-                const std::vector<double> theta_3 = sp3_t3.get_theta();
-
-                for(const auto& q3 : theta_3)
-                {                
-                    const Eigen::Matrix3d rot_1 = Eigen::AngleAxisd(q3, this->H.col(1).normalized()).toRotationMatrix();
-                    SP1 sp1_q2(this->P.col(2)+rot_1*this->P.col(3), d_inner, this->H.col(1));
-                    sp1_q2.solve();
-
-                    double q4 = sp_14.get_theta() - sp1_q2.get_theta() - q3;
-                    q4 = std::atan2(std::sin(q4), std::cos(q4)); // Map angle q4 to [-PI,PI)
-
-                    solution.Q.push_back({q1, sp1_q2.get_theta(), q3, q4, q5, sp_q6.get_theta()});
-                    solution.is_LS_vec.push_back(is_ls_q1_q5.at(i) || sp1_q2.solution_is_ls() || sp3_t3.solution_is_ls() ||sp_q6.solution_is_ls());
+                if(reversed_Robot_ptr)
+                {
+                    // Use reversed kinematic chain and calculate IK for robot with h2 || h3 || h4
+                    solution = this->reversed_Robot_ptr->calculate_IK(inverse_homogeneous_T(ee_position_orientation));
+                    reverse_vector_second_dimension(solution.Q);
                 }
-            }
-        }
-        else if (this->H.col(2).cross(this->H.col(3)).norm() < ZERO_THRESH &&
-                 this->H.col(2).cross(this->H.col(4)).norm() < ZERO_THRESH &&
-                 this->H.col(3).cross(this->H.col(4)).norm() < ZERO_THRESH)
-        {
-            // h3 || h4 || h5
-            // Reverse kinematic chain and calculate IK for robot with h2 || h3 || h4
-            const auto&[H_reversed, P_reversed] = reverse_kinematic_chain(this->H, this->P);
-            General_6R reversed_robot (H_reversed, P_reversed);
-
-            solution = reversed_robot.calculate_IK(inverse_homogeneous_T(ee_position_orientation));
-
-            reverse_vector_second_dimension(solution.Q);
-        }
-        else if (this->H.col(3).cross(this->H.col(4)).norm() < ZERO_THRESH &&
-                 this->H.col(3).cross(this->H.col(5)).norm() < ZERO_THRESH &&
-                 this->H.col(4).cross(this->H.col(5)).norm() < ZERO_THRESH)
-        {
-            // h4 || h5 || h6
-            // Reverse kinematic chain and calculate IK for robot with h1 || h2 || h3
-            const auto&[H_reversed, P_reversed] = reverse_kinematic_chain(this->H, this->P);
-            General_6R reversed_robot (H_reversed, P_reversed);
-
-            solution = reversed_robot.calculate_IK(inverse_homogeneous_T(ee_position_orientation));
-            reverse_vector_second_dimension(solution.Q);
+                else
+                {
+                    throw std::runtime_error("Reversed kinematic chain was not initialized! Please open an issue on our GitHub repository.");
+                }
+                
+                break;
+            };
+            default:
+                std::cerr<< "The choosen manipulator has no known subproblem decomposition! The resulting solutions will be empty."<<std::endl;
         }
 
         return solution;
--- CPP/src/IK/IKS.h
+++ CPP/src/IK/IKS.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <eigen3/Eigen/Dense>
+#include <memory>
 
 namespace IKS
 {
@@ -31,6 +32,7 @@ namespace IKS
         virtual Homogeneous_T fwdkin(const std::vector<double> &Q) const final;
 
         virtual bool has_known_decomposition() const;
+
     private:
         Eigen::MatrixXd H;
         Eigen::MatrixXd P;
@@ -43,7 +45,7 @@ namespace IKS
         General_3R(const Eigen::Matrix<double, 3, 3> &H, const Eigen::Matrix<double, 3, 4> &P);
         IK_Solution calculate_IK(const Homogeneous_T &ee_position_orientation) const override;
 
-        bool has_known_decomposition() const override {return true;}
+        bool has_known_decomposition() const override { return true; }
 
     private:
         Eigen::Matrix<double, 3, 3> H;
@@ -60,18 +62,33 @@ namespace IKS
         bool has_known_decomposition() const override;
 
     private:
+        enum KinematicClass
+        {
+            THREE_INNER_PARALLEL = 0,
+            THREE_INNER_PARALLEL_REVERSED = 1,
+            THREE_PARALLEL_TWO_INTERSECTING = 2,
+            THREE_PARALLEL_TWO_INTERSECTING_REVERSED = 3,
+            UNKNOWN
+        };
+
+        KinematicClass determine_Kinematic_Class();
+
         Eigen::Matrix<double, 3, 6> H;
         Eigen::Matrix<double, 3, 7> P;
+
+        KinematicClass kinematicClass{KinematicClass::UNKNOWN};
+
+        std::unique_ptr<General_6R> reversed_Robot_ptr;   // If kinematic class demands kinematic inversion, this robot will be used
     };
 
     class Spherical_Wrist_Robot : public General_Robot
     {
         // 6DOF Robot kinematics with spherical wrist (3 consecutive intersecting axes at the endeffector)
     public:
-        Spherical_Wrist_Robot(const Eigen::Matrix<double, 3, 6> &H, const Eigen::Matrix<double, 3, 7> &P, const bool use_inverted_chain=false);
+        Spherical_Wrist_Robot(const Eigen::Matrix<double, 3, 6> &H, const Eigen::Matrix<double, 3, 7> &P, const bool use_inverted_chain = false);
         IK_Solution calculate_IK(const Homogeneous_T &ee_position_orientation) const override;
 
-        bool has_known_decomposition() const override {return true;}
+        bool has_known_decomposition() const override { return true; }
 
     private:
         Eigen::Matrix<double, 3, 6> H;
